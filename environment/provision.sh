3997ba945e13472ecb38c0c4464c93e9fbd4c6f2
Jenny Johnson <kippz07@users.noreply.github.com>
2017-08-10 15:01:48 +0100

    dev environment complete

---
+++ environment/provision.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# install nginx
+sudo apt-get update
+sudo apt-get install nginx -y
+
+# install nodejs
+curl -sL https://deb.nodesource.com/setup_7.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt-get install -y nodejs
+sudo apt-get install -y build-essential
+
+sudo npm install -g pm2
+
+sudo rm /etc/nginx/sites-available/default
+sudo cp /home/ubuntu/app/environment/templates/default /etc/nginx/sites-available/default
+sudo service nginx reload
